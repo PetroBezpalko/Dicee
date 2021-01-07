9762d6eaac28e048380668316f94249ab671b415
PetroBezpalko <74530722+PetroBezpalko@users.noreply.github.com>
2021-01-07 13:40:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-var started = false;
+document.addEventListener("click", function() {
+  var randomNumber = Math.ceil(Math.random() * 6);
+  var randomNumber2 = Math.ceil(Math.random() * 6);
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-document.addEventlistener("click"), function() {
-  if (!started) {
-    started = true;
+  if (randomNumber > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+  } else if (randomNumber < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
   } else {
-
-    var randomNumber = Math.ceil(Math.random() * 6);
-    var randomNumber2 = Math.ceil(Math.random() * 6);
-    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber + ".png");
-    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-    if (randomNumber > randomNumber2) {
-      document.querySelector("h1").textContent = "Player 1 Wins!";
-    } else if (randomNumber < randomNumber2) {
-      document.querySelector("h1").textContent = "Player 2 Wins!";
-    } else {
-      document.querySelector("h1").textContent = "Draw!";
-    }
+    document.querySelector("h1").textContent = "Draw!";
   }
 });
