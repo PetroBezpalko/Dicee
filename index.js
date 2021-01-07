56c500b2595d78ac3f3729959bfcef297ac70461
PetroBezpalko <74530722+PetroBezpalko@users.noreply.github.com>
2021-01-07 13:08:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var randomNumber=Math.ceil(Math.random()*6);
-var randomNumber2=Math.ceil(Math.random()*6);
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber+".png");
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+var started = false;
 
-if (randomNumber>randomNumber2){
-  document.querySelector("h1").textContent="Player 1 Wins!";
- } else if (randomNumber<randomNumber2) {
-  document.querySelector("h1").textContent="Player 2 Wins!";
-} else {
-  document.querySelector("h1").textContent="Draw!";
-}
+$("body").click(function() {
+  if (!started) {
+    started = true;
+  } else {
+
+    var randomNumber = Math.ceil(Math.random() * 6);
+    var randomNumber2 = Math.ceil(Math.random() * 6);
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber > randomNumber2) {
+      document.querySelector("h1").textContent = "Player 1 Wins!";
+    } else if (randomNumber < randomNumber2) {
+      document.querySelector("h1").textContent = "Player 2 Wins!";
+    } else {
+      document.querySelector("h1").textContent = "Draw!";
+    }
+  }
+});
